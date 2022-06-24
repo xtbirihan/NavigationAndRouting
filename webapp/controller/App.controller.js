7826915784a84a5943c753f47294f3cf4c62d06c
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-24 09:46:39 +0000

    Sample: Navigation and Routing - Step 17 - Listen to Matched Events of Any Route

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -15,6 +15,13 @@ sap.ui.define([
 				var sHash = oEvent.getParameter("hash");
 				Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
 			});
+			oRouter.attachRouteMatched(function (oEvent){
+				var sRouteName = oEvent.getParameter("name");
+				// do something, i.e. send usage statistics to back end
+				// in order to improve our app and the user experience (Build-Measure-Learn cycle)
+				Log.info("User accessed route " + sRouteName + ", timestamp = " + new Date().getTime());
+			});
+
 		}
 
 	});
