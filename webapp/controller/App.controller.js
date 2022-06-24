81598a74cf36879c3918936b8caa5ba3d069090a
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-24 09:43:01 +0000

    Sample: Navigation and Routing - Step 16 - Handle Invalid Hashes by Listening to Bypassed Events

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,20 @@
 sap.ui.define([
-	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
+	"sap/ui/demo/nav/controller/BaseController",
+    "sap/base/Log"
+], function (BaseController, Log) {
 	"use strict";
 
 	return BaseController.extend("sap.ui.demo.nav.controller.App", {
 
 		onInit: function () {
+			Log.setLevel(Log.Level.INFO);
 
+			var oRouter = this.getRouter();
+
+			oRouter.attachBypassed(function (oEvent) {
+				var sHash = oEvent.getParameter("hash");
+				Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+			});
 		}
 
 	});
