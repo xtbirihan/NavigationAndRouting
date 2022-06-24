e5d5aa247cc027568f901b94bfa393cce7d762f2
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-24 08:08:46 +0000

    Sample: Navigation and Routing - Step 9 - Allow Bookmarkable Tabs with Optional Query Parameters

--- webapp/controller/employee/Resume.controller.js
+++ webapp/controller/employee/Resume.controller.js
@@ -1,15 +1,18 @@
 sap.ui.define([
-	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
+	"sap/ui/demo/nav/controller/BaseController",
+    "sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
 	"use strict";
+    var _aValidTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
 	return BaseController.extend("sap.ui.demo.nav.controller.employee.Resume", {
         onInit: function () {
             var oRouter = this.getRouter();
+            this.getView().setModel(new JSONModel() , "view"); 
             oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this); 
         },
 
         _onRouteMatched : function (oEvent) {
-            var oArgs, oView;
+            var oArgs, oView, oQuery;
             oArgs = oEvent.getParameter("arguments"); 
             oView = this.getView(); 
             var fnSetBusy = function (bBusyIndicator) {
@@ -23,12 +26,34 @@ sap.ui.define([
                      dataReceived:  fnSetBusy( false )
                 }
             });
+
+            oQuery = oArgs["?query"]; 
+            if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1 ) {
+                oView.getModel("view").setProperty("/selectedTabKey", oQuery.tab);  
+            } else {
+                this.getRouter().navTo("employeeResume" , {
+                    employeeId: oArgs.employeeId,
+                    "?query": {
+                        tab: _aValidTabKeys[0]
+                    }
+                }, true);
+            }
         },
         _onBindingChange : function (oEvent) {
 			// No data for the binding
 			if (!this.getView().getBindingContext()) {
 				this.getRouter().getTargets().display("notFound");
 			}
-		}
+		},
+        onTabSelect: function (oEvent){ 
+            var oCtx = this.getView().getBindingContext();
+            this.getRouter().navTo("employeeResume", {
+				employeeId: oCtx.getProperty("EmployeeID"),
+				"?query": {
+					tab: oEvent.getParameter("selectedKey")
+				}
+            }, true );
+
+        }
 	});
 });
